2b2be64fee155d358195a7bf02265d8c55dafa5a
channkenn <91776275+channkenn@users.noreply.github.com>
2024-12-05 12:05:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,27 @@ def index():
     thread_url = None
     thread_title = None
     images = []
+    warning_message = None  # 警告メッセージを格納する変数
+
     if request.method == "POST":
+        # ユーザー入力URL
         thread_url = request.form.get("url")
         if thread_url:
-            thread_title, images = fetch_images_and_title(thread_url)
-    return render_template("index.html", thread_url=thread_url, thread_title=thread_title, images=images)
+            # URLがあにまん掲示板か確認
+            if "https://bbs.animanch.com/" not in thread_url:
+                warning_message = "あにまん掲示板でおねがいします"
+                thread_url = "https://bbs.animanch.com/"
+            else:
+                # スレッドタイトルと画像を取得
+                thread_title, images = fetch_images_and_title(thread_url)
+
+    return render_template(
+        "index.html",
+        thread_url=thread_url,
+        thread_title=thread_title,
+        images=images,
+        warning_message=warning_message,
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
